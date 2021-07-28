52fc777c9bd1b23588f1bef7d226548ffa7bc10d
Roman Samolazov <samolazoff.roman@yandex.by>
2021-07-28 16:02:55 +0300

    feet: add new version  js

--- src/index.js
+++ src/index.js
@@ -3,64 +3,67 @@ function eval() {
     return;
 }
 function expressionCalculator(expr) {
-    expr=expr.replace(/\s/g,'');
-    function delition(expr){
+  expr=expr.replace(/\s/g,'');
+  function delition(expr){
+    if(/\d/.test(expr)){
       while(/\//.test(expr)){
         let arr=expr.match(/(\-?\d+\.\d+|\-?\d+)\/(\-?\d+\.\d+|\-?\d+)/);
         if(arr[2]==0){
-        expr="TypeError: Division by zero.";
-        break;}
+        throw "TypeError: Division by zero.";
+     }
         expr=expr.replace(/(\-?\d+\.\d+|\-?\d+)\/(\-?\d+\.\d+|\-?\d+)/,Number(arr[1])/Number(arr[2]))
+      }
     }
     return expr
-    }
-    function mult(expr){
+  }
+  function mult(expr){
+    if(/\d/.test(expr)){
       while(/\*/.test(expr)){
         let arr=expr.match(/(\-?\d+\.\d+|\-?\d+)\*(\-?\d+\.\d+|\-?\d+)/);
         expr=expr.replace(/(\-?\d+\.\d+|\-?\d+)\*(\-?\d+\.\d+|\-?\d+)/,Number(arr[1])*Number(arr[2]))
     }
-    return expr
-    }
-    function sum(expr){
-      while(/\+/.test(expr)){
-        let arr=expr.match(/(\-?\d+\.\d+|\-?\d+)\+(\-?\d+\.\d+|\-?\d+)/);
-        expr=expr.replace(/(\-?\d+\.\d+|\-?\d+)\+(\-?\d+\.\d+|\-?\d+)/,Number(arr[1])+Number(arr[2]))
-    }
-    return expr
-    }
-    function minus(expr){
-      while(/\d\-\d/.test(expr)){
-        let arr=expr.match(/(\-?\d+\.\d+|\-?\d+)\-(\-?\d+\.\d+|\-?\d+)/);
-        expr=expr.replace(/(\-?\d+\.\d+|\-?\d+)\-(\-?\d+\.\d+|\-?\d+)/,Number(arr[1])-Number(arr[2]))
-    }
-    return expr
-    }
-    function action(expr){
-      return minus(sum(mult(delition(expr))))
-    }
-    function brck(expr){
-      while(/\(.+\)/.test(expr)){
-        //   let wrd=expr.match(/\(.+\)/)[0];
-        //   wrd=wrd
-        expr=expr.replace(/\(.+\)/,action(expr.match(/\(.+\)/)[0].replace(/\(/g,'').replace(/\)/g,'')))
+  }
+  
+  return expr
+  }
+  function sum(expr){
+    if(/\d/.test(expr)){
+      let rez=0;
+      expr.match(/\-?\d+\.\d+|\-?\d+/g)
+        .forEach(element => {
+            rez+=Number(element)
+          });
+      expr=rez;
       }
-      return expr
+      return expr;
+  }
+  function action(expr){
+    return sum(mult(delition(expr)));
+  }
+  function brck(expr){
+    while(/\(.+\)/.test(expr)){
+      expr.match(/\(([^()]+)\)/)
+        expr=expr.replace(/\(([^()]+)\)/,action(expr.match(/\(([^()]+)\)/)[0].replace(/\(/,'').replace(/\)/,'')))
     }
-    function run(expr){
-      if(expr.match(/\(/g)!=null&&expr.match(/\)/g)!=null){
-        if(expr.match(/\(/g).length==expr.match(/\)/g).length){
-          return action(brck(expr));
-        }else{
-          return "ExpressionError: Brackets must be paired";
-        }
-      }else{if(expr.match(/\(/g)===null||expr.match==null){
-            return action(expr);
+    return expr
+  }
+  function run(expr){
+    if(expr.match(/\(/g)!=null&&expr.match(/\)/g)!=null){
+      if(expr.match(/\(/g).length==expr.match(/\)/g).length){
+        return action(brck(expr));
       }else{
-        return "ExpressionError: Brackets must be paired";
-      };
-    }
-    }
-    return /\d/.test(run(expr))?Number(run(expr)):run(expr)
+        throw "ExpressionError: Brackets must be paired";
+      }
+    }else{if(expr.match(/\(/g)===null&&expr.match(/\)/g)==null){
+          return action(expr);
+    }else{
+      throw "ExpressionError: Brackets must be paired";
+    };
+  }
+  }
+  if(/\d/.test(run(expr))){
+    return Number(run(expr));
+  }else{ return run(expr);}
 }
 module.exports = {
     expressionCalculator
